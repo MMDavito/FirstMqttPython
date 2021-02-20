d44fd350103248ed5aa42ff7f8e4a51e387e1280
MMDavito <davidking9753@gmail.com>
2021-02-20 13:25:52 +0100

    Will next time get an http-bridge or something. So I can post turn-on-light using an http-request. Will thereafter set up arduino to turn-on led when said so and turn off when set false.
    Will look into conformations of change and then:
    Thereafter create an webpage where you can turn-on led and off led and get switch only if it turns on or off.
    Will after this start looking at temperature relay controller (keep temperature in range)
    Check temperature against low or highest, each 10- min, sec or hour.
    So one does not turn on and off relay/cupe-warmer to often, but that one can adjust to makeing beer, that requires more accoracy/nogranhet.
    
    Will now eat

--- Server/FirstMqttSubscriber.py
+++ Server/FirstMqttSubscriber.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-
-
 import random
-import time
 
 from paho.mqtt import client as mqtt_client
 
 
 broker = 'broker.emqx.io'
 port = 1883
-topic = "/python/mqtt"
+topic = "python/mqtt"
 # generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
+client_id = f'python-mqtt-{random.randint(0, 100)}'
 # username = 'emqx'
 # password = 'public'
 
-def connect_mqtt():
+
+def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
@@ -29,25 +27,18 @@ def connect_mqtt():
     return client
 
 
-def publish(client):
-    msg_count = 0
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
 
 
 def run():
     client = connect_mqtt()
-    client.loop_start()
-    publish(client)
+    subscribe(client)
+    client.loop_forever()
 
 
 if __name__ == '__main__':
